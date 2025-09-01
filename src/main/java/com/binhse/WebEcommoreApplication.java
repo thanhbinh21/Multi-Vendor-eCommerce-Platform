b2ac145ee4366bfd207754609aca883d67f79b3b
thanhbinh21 <iambinhage17@gmail.com>
2025-09-01 18:37:12 +0700

    first commit

---
+++ src/main/java/com/binhse/WebEcommoreApplication.java
@@ -0,0 +1,15 @@
+package com.binhse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEcommoreApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WebEcommoreApplication.class, args);
+		System.out.println("Heloooooooooooooooo");
+	}
+
+}
